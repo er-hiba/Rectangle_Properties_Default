eea91eaea066a06440143a625eb75f83c396ea4f
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-11-19 14:16:21 +0100

    :)

---
+++ main.py
@@ -0,0 +1,10 @@
+# Importing the 'Rectangle' class from the file 'rectangle2'
+from rectangle2 import Rectangle
+
+# Creating an instance of the 'Rectangle' class using a default constructor
+rectangle = Rectangle()   
+
+print("Rectangle Properties : ")
+
+# Displaying the properties of the rectangle using the 'DisplayRectangle' method
+rectangle.DisplayRectangle()
